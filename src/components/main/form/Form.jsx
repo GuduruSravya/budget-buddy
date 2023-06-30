a561674214ee70f970b8fe46eb75df4ed1223131
Sravya <gudursravya@gmail.com>
2023-06-30 15:49:22 -0500

    Added functionality to format date

--- src/components/main/form/Form.jsx
+++ src/components/main/form/Form.jsx
@@ -2,6 +2,7 @@ import React,{useState,useContext} from 'react';
 import {TextField,Typography,Grid,Button,FormControl,InputLabel,Select,MenuItem} from '@material-ui/core';
 import { BudgetBuddyContext } from '../../../context/context';
 import {v4 as uuidv4} from 'uuid';
+import formatDate from '../../../utils/formatDate';
 import { incomeCategories,expenseCategories } from '../../../constants/categories';
 import useStyles from './styles'
 
@@ -18,7 +19,7 @@ const Form = () =>{
         type:'Income',
         category:'',
         amount:'',
-        date:new Date()
+        date:formatDate(new Date())
     }
 
     
@@ -61,7 +62,7 @@ const Form = () =>{
         <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e)=>{setFormData({...formData,amount:e.target.value})}}/>
     </Grid>
     <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e)=>{setFormData({...formData,date:e.target.value})}}/>
+        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e)=>{setFormData({...formData,date:formatDate(e.target.value)})}}/>
     </Grid>
     <Button variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
 
