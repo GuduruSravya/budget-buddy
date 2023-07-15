70ebf7d2a02e99ebdfe94455ac384c91f80fb624
Sravya <gudursravya@gmail.com>
2023-07-15 12:28:04 -0500

    "Implemented storing in localstorage and also calculated balance"

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Card,CardHeader,CardContent,Typography,Grid,Divider} from '@material-ui/core';
 import Form from './form/Form';
 import List from './list/List';
+import { BudgetBuddyContext } from '../../context/context';
 
 const Main = () =>{
+    const {balance} = useContext(BudgetBuddyContext);
     return(
       <Card>
         <CardHeader title="Budget Buddy" subheader="Powered by speechly"/>
         <CardContent>
-            <Typography align="center" variant='h5'>Total Balance $500</Typography>
+            <Typography align="center" variant='h5'>Total Balance ${balance}</Typography>
             <Typography variant="subtitle1" style={{lineHeight:'1.5rem',marginTop:'20px'}}>
                Try saying: Add income for $100 in category salary for monday...
             </Typography>
